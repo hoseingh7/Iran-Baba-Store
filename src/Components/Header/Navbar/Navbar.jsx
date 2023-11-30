eb3e3131a80c214c860d43f21d6dce42851fbeaa
hoseingh7 <hoseingholamii1376@gmail.com>
2023-11-30 14:32:51 -0800

    adding navbar and menu-list

---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div className=" pb-3">
+        <button className="bg-white text-[14px] border border-none">
+          دسته‌بندی کالاها
+        </button>
+        <button className="bg-white text-[14px] border border-none">
+          تماس با ما
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
