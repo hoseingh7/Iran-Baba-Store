78d3d44b553c7bba43376d496cbd6f6211816c31
hoseingh7 <hoseingholamii1376@gmail.com>
2023-12-06 05:49:59 -0800

    ...

--- src/Layout/Router.jsx
+++ src/Layout/Router.jsx
@@ -1,8 +1,8 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+// import Header from "../components/Header/Header";
+// import Footer from "../components/Footer/Footer";
 // import { Home } from "../Pages/Home";
 import LoadingSpinner from "../components/LoadinSpinner/LoadingSpinner";
 import Layout from "./Layout";
