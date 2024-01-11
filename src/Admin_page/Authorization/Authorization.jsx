2ac90e3ed09186e8195376af0e4164c8a1a58ea4
hoseingh7 <hoseingholamii1376@gmail.com>
2024-01-11 02:54:32 -0800

    editing Authorization for access token

---
+++ src/Admin_page/Authorization/Authorization.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (accessToken) => {
+    setUser({ accessToken });
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth باید در داخل AuthProvider استفاده شود");
+  }
+  return context;
+};
