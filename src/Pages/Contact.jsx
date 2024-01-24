73c435ccb28e885f6c1416f24b68e7a61a147fbf
hoseingh7 <hoseingholamii1376@gmail.com>
2024-01-24 15:28:20 -0800

    copleting the contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,97 @@
 import React from "react";
+import Asqar from "../assets/Contact/contact.svg";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="w-full bg-[#fafafa] flex flex-col border-t border-b-2 gap-8  py-8 border-gray-300">
+      <div className="w-4/5 m-auto">
+        <img
+          src={Asqar}
+          alt=""
+        />
+      </div>
+      <div className="w-4/5 m-auto flex flex-col py-2">
+        <h1 className="text-start text-2xl font-bold pb-6 ">
+          تماس با ایران بابا
+        </h1>
+        <p className="text-[13px] ">
+          تماس با ایران بابا، شروع یک گفتگوی دوستانه در جهت پرسش سؤالات، دریافت
+          مشاوره خرید از آمازون یا سایر سایت‌های خارجی، و بیان هر گونه انتقاد و
+          پیشنهاد می‌باشد. تماس با ایران بابا از طریق روش‌های مختلفی از
+          امکان‌پذیر است که پیام در واتساپ و تماس تلفنی محبوب‌ترین آن‌ها هستند.
+          در صورتی که قصد خرید کالای سفارشی از سایت‌های خارجی را دارید،
+          کارشناسان فروش ایران بابا می‌توانند با صبوری شما را در این مسیر
+          راهنمایی نمایند تا در صورت تمایل کالاهای مد نظرتان را انتخاب نمایید تا
+          خرید آن را برایتان انجام دهند.
+        </p>
+      </div>
+
+      <div className="w-full m-auto flex items-start flex-col gap-5">
+        <div className="flex  w-full items-start ">
+          <div className="flex w-full items-center justify-center gap-3">
+            <div className="w-[50px] h-[50px]">
+              <svg
+                className="w-[50px] h-[50px]"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="bi bi-building"
+                viewBox="0 0 16 16">
+                <path
+                  fill-rule="evenodd"
+                  d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"></path>
+                <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"></path>
+              </svg>
+            </div>
+            <div className="text-start pt-3">
+              <h1 className="text-[20px] text-gray-400">دفتر مشهد:</h1>
+              <p>91009811-051</p>
+            </div>
+          </div>
+          <div className="flex w-full items-center justify-center gap-3">
+            <div className="w-[50px] h-[50px]">
+              <svg
+                className="w-[50px] h-[50px]"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="bi bi-building"
+                viewBox="0 0 16 16">
+                <path
+                  fill-rule="evenodd"
+                  d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"></path>
+                <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"></path>
+              </svg>
+            </div>
+            <div className="text-start pt-3">
+              <h1 className="text-[20px] text-gray-400">دفتر مشهد:</h1>
+              <p>91009811-051</p>
+            </div>
+          </div>
+          <div className="flex w-full items-center justify-center gap-3">
+            <div className="w-[50px] h-[50px]">
+              <svg
+                className="w-[50px] h-[50px]"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="bi bi-building"
+                viewBox="0 0 16 16">
+                <path
+                  fill-rule="evenodd"
+                  d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"></path>
+                <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"></path>
+              </svg>
+            </div>
+            <div className="text-start pt-3">
+              <h1 className="text-[20px] text-gray-400">دفتر مشهد:</h1>
+              <p>91009811-051</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-4/5 mx-auto">
+          <p>آدرس دفتر مشهد: بولوار دانشجو، نبش دانشجو 23، پلاک 555، طبقه 4</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
