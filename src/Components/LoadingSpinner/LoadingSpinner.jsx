33dbac4c3fe24e9879991de80d805c4124a9804e
hoseingh7 <hoseingholamii1376@gmail.com>
2024-01-24 15:11:23 -0800

    completion loading spinner site

---
+++ src/Components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoadingSpinner() {
+  return (
+    <div className="w-4/5 m-auto">
+      <h1>... Loading </h1>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
